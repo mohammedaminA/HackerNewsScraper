cfd62636c0aa9f495b3a05f95496f2590b076cd0
Mohammedamin Abdullah <59562112+mohammedaminA@users.noreply.github.com>
2020-09-16 00:35:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.storylink')
 subtext = soup.select('.subtext')
 
-
+# sorts by descending order of votes
 def sort_stories_by_votes(hnlist):
     return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
 
 
 def create_custom_hn(links, subtext):
     hn = []
+    # loop through links and titles
     for index, item in enumerate(links):
         title = links[index].getText()
         href = links[index].get('href', None)
         vote = subtext[index].select('.score')
+        # only add stories having at least a hundred votes
         if len(vote):
             points = int(vote[0].getText().replace(' points', ''))
             if points > 99:
